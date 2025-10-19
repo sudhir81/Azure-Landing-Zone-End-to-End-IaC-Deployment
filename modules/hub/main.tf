7578b6b00837608f128160b57d8d04db10ae7857
sudhir81 <strangebiker@gmail.com>
2025-10-20 00:00:15 +0530

    ✅ Successful landing zone deployment - hub & spoke complete

--- modules/hub/main.tf
+++ modules/hub/main.tf
@@ -74,9 +74,3 @@ resource "azurerm_monitor_diagnostic_setting" "afw_diag" {
     enabled  = true
   }
 }
-
-output "rg_name" { value = azurerm_resource_group.hub_rg.name }
-output "vnet_id" { value = azurerm_virtual_network.hub_vnet.id }
-output "vnet_name" { value = azurerm_virtual_network.hub_vnet.name }
-output "route_table_id" { value = azurerm_route_table.rt_to_fw.id }
-output "firewall_private_ip" { value = azurerm_firewall.afw.ip_configuration[0].private_ip_address }
